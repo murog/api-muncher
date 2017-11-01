1ee1830cbe3f2efcdfa3497f8d708cb930ac5277
murog <guillermina.muro@gmail.com>
2017-11-01 10:27:37 -0700

    configured test helper to include vcr, added test for api wrapper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,20 @@ require "rails/test_help"
 require "minitest/rails"
 require "minitest/reporters"  # for Colorized output
 
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes' # folder where casettes will be located
+  config.hook_into :webmock # tie into this other tool called webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,    # record new data when we don't have it yet
+    :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
+  }
+  # Don't leave our Slack token lying around in a cassette file.
+  config.filter_sensitive_data("<EDAMOM_TOKEN>") do
+    ENV['EDAMOM_TOKEN']
+  end
+end
+
+
 #  For colorful output!
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
