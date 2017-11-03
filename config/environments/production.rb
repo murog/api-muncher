c95e0356570f7906b021058612ce858e625618fd
murog <guillermina.muro@gmail.com>
2017-11-03 13:08:00 -0700

    added config lines to fix heroku issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  #gale told me to
+  config.enable_dependency_loading = true
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
