1b8c44e56fcafb4a3f7f94aac9a28c1e785bcb91
murog <guillermina.muro@gmail.com>
2017-11-02 22:22:40 -0700

    pages are working through sent params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,21 @@
 class RecipesController < ApplicationController
   def search
+    # recipe_data["info"] = ({more: data["more"], count: data["count"], url: url})
     @results  = EdamamApiWrapper.list_recipes(params)
     @recipes = @results["recipes"]
+    @more = @results["info"][:more]
+    @count = @results["info"][:count].to_i
+    @pages = @count/10
+    @from = params["from"]
+    @to = params["to"]
+    @current_page = 1
+  end
+  def pages
+    @current_page = params["id"].to_i
+    @pages = params[:pages].to_i
+    @results  = EdamamApiWrapper.list_recipes(params)
+    @recipes = @results["recipes"]
+    @from = params[:from]
+    @to = params[:to]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'application#index'
+get 'search/:id', to: "recipes#pages", as: "pages"
 post 'search', to: 'recipes#search', as: 'search'
 get '/results', to: 'recipes#results', as: 'results'
 
