5eddcd1544b6bab605d148e073eda9b2b42c824f
murog <guillermina.muro@gmail.com>
2017-11-04 13:39:02 -0700

    added render 404 to show action in recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,15 @@ class RecipesController < ApplicationController
   end
 
   def show
-    
+    @results = EdamamApiWrapper.find_recipe(params[:id])
+    if !(@results)
+      render_404
+    end
+
+  end
+  private
+  def render_404
+    render file: "/public/404.html", status: 404
+    return
   end
 end
