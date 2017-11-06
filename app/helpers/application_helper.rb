6dd1711e2514f5ee1c37862f57ede7136a5b2347
murog <guillermina.muro@gmail.com>
2017-11-06 00:52:17 -0800

    random image helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def random_image_url
+    images = ["bubbles.gif", "egg.gif", "hamtaro.gif", "jam.gif", "ramen.gif", "squirtle.gif", "stirring.gif"]
+    images.shuffle!
+    return images.pop
+  end
 end
