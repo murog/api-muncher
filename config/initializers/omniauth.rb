d1e9c3229c9e20fff0b607a80cf98e7c23b4766f
murog <guillermina.muro@gmail.com>
2017-11-06 10:35:55 -0800

    updated omniauth to correct domain name for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
-  OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:3000'
+  OmniAuth.config.full_host = Rails.env.production? ? 'https://gm-muncher.herokuapp.com' : 'http://localhost:3000'
 
 end
