1c9af5448f430a95e6f309913e48b07e09f1e400
murog <guillermina.muro@gmail.com>
2017-11-06 05:05:21 -0800

    added search bar and edamam credit, added view helper to truncate recipe name if it's too long

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     images.shuffle!
     return images.pop
   end
+
+  def format_length(string)
+    if string.length > 64
+      return "#{string[0..64]}..."
+    else
+      return string
+    end
+  end
 end
